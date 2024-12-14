e09ae5f838b996586bd59d4522384368bd8209bb
Sajal <newtonh48@gmail.com>
2024-12-15 00:09:53 +0600

    orderService implementation complete without saga

---
+++ order-service/order-domain/order-application-service/src/main/java/com/sajal48/order/application/PaymentResponseMessageListenerImpl.java
@@ -0,0 +1,22 @@
+package com.sajal48.order.application;
+
+import com.sajal48.order.application.dto.message.PaymentResponse;
+import com.sajal48.order.application.ports.input.message.listener.payment.PaymentResponseMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+    @Override
+    public void paymentCompleted(PaymentResponse paymentResponse) {
+
+    }
+
+    @Override
+    public void paymentCancelled(PaymentResponse paymentResponse) {
+
+    }
+}
